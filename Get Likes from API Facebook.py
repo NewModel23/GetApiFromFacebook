db8d6e2a7a6b53e75dc49022ca2944e5f4622e51
Raul Guerrero <32245710+NewModel23@users.noreply.github.com>
2019-04-03 16:33:05 -0600

    Add files via upload

--- Get Likes from API Facebook.py
+++ Get Likes from API Facebook.py
@@ -4,6 +4,8 @@ from urllib.request import Request, urlopen
 import json
 import pandas as pd
 
+# https://developers.facebook.com/tools/explorer/?method=GET&path=me%3Ffields%3Did%2Cname%2Cbirthday%2Clikes&version=v3.2
+
 token = ''
 request=Request('https://graph.facebook.com/v3.0/me?fields=id,name,likes&access_token=' + token)
 response = urlopen(request)
